d3ff25fca53b181828471ee05437883690d655b7
KaustubhG <kaustubh.gondhalekar22@gmail.com>
2017-03-07 17:01:55 -0500

    UI improvements

--- app/src/main/java/teamh/zapapp/ProfileActivity.java
+++ app/src/main/java/teamh/zapapp/ProfileActivity.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import okhttp3.OkHttpClient;
@@ -31,6 +32,9 @@ public class ProfileActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_profile);
         bt_Logout = (Button) findViewById(R.id.btn_logout);
         context = getApplicationContext();
